d3699a9bc5026e110ac53c868613673e648e97a1
Sailesh <saileshr2209@gmail.com>
2021-04-10 08:58:10 +0530

    added new typedef and resolver to get posts

--- index.js
+++ index.js
@@ -6,14 +6,28 @@ const { MONGODB } = require("./config");
 const Post = require("./models/Post");
 
 const typeDefs = gql`
+  type Post {
+    id: ID!
+    body: String!
+    createdAt: String!
+    username: String!
+  }
+
   type Query {
-    sayHi: String!
+    getPosts: [Post]
   }
 `;
 
 const resolvers = {
   Query: {
-    sayHi: () => "Hello world!",
+    async getPosts() {
+      try {
+        const posts = await Post.find();
+        return posts;
+      } catch (err) {
+        throw new Error(err);
+      }
+    },
   },
 };
 
