8eb862ace909d9158f80c6f41b8adf1af8317655
Sailesh <saileshr2209@gmail.com>
2021-04-10 17:22:08 +0530

    added login functionality along with error checking

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -4,10 +4,47 @@ const jwt = require("jsonwebtoken");
 const { UserInputError } = require("apollo-server");
 
 const { SECRET_KEY } = require("../../config");
-const { validateRegisterInput } = require("../../utils/validators");
+const {
+  validateRegisterInput,
+  validateLoginInput,
+} = require("../../utils/validators");
+
+const generateToken = (user) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    },
+    SECRET_KEY,
+    { expiresIn: "1h" }
+  );
+};
 
 module.exports = {
   Mutation: {
+    async login(_, { username, password }) {
+      const { errors, valid } = validateLoginInput(username, password);
+      const user = await User.findOne({ username });
+      if (!valid) {
+        throw new UserInputError("Errors", { error });
+      }
+      if (!user) {
+        errors.general = "User not found";
+        throw new UserInputError("User not found", { error });
+      }
+      const match = await bcrypt.compare(password, user.password);
+      if (!match) {
+        errors.general = "Wrong Credentials";
+        throw new UserInputError("Wrong Credentials", { error });
+      }
+      const token = generateToken(user);
+      return {
+        ...user._doc,
+        id: user._id,
+        token,
+      };
+    },
     async register(
       _,
       { registerInput: { username, password, confirmPassword, email } }
@@ -47,15 +84,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign(
-        {
-          id: res.id,
-          email: res.email,
-          username: res.username,
-        },
-        SECRET_KEY,
-        { expiresIn: "1h" }
-      );
+      const token = generateToken(res);
 
       return {
         ...res._doc,
