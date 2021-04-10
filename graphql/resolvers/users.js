58ee07ce6f31cf140197ba5414679823d2b8af35
Sailesh <saileshr2209@gmail.com>
2021-04-10 15:36:22 +0530

    added functionality to register users and store them in database

---
+++ graphql/resolvers/users.js
@@ -0,0 +1,46 @@
+const User = require("../../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const { SECRET_KEY } = require("../../config");
+
+module.exports = {
+  Mutation: {
+    async register(
+      _,
+      { registerInput: { username, password, confirmPassword, email } },
+      context,
+      info
+    ) {
+      // validate user data
+      // make sure user doesn't already exist
+      // hash password and create an auth token
+      password = await bcrypt.hash(password, 12);
+
+      const newUser = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString(),
+      });
+
+      const res = await newUser.save();
+
+      const token = jwt.sign(
+        {
+          id: res.id,
+          email: res.email,
+          username: res.username,
+        },
+        SECRET_KEY,
+        { expiresIn: "1h" }
+      );
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token,
+      };
+    },
+  },
+};
