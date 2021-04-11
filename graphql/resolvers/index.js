9c291d6daabba2351a3afb3c7e4802f918b86388
Sailesh <saileshr2209@gmail.com>
2021-04-11 13:09:04 +0530

    added likecount and commentcount

--- graphql/resolvers/index.js
+++ graphql/resolvers/index.js
@@ -4,6 +4,10 @@ const commentsResolvers = require("../resolvers/comments");
 const likeResolvers = require("../resolvers/likes");
 
 module.exports = {
+  Post: {
+    likeCount: (parent) => parent.likes.length,
+    commentCount: (parent) => parent.comments.length,
+  },
   Query: {
     ...postsResolvers.Query,
   },
